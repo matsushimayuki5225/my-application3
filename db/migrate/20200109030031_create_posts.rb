5c42b45fe89daec1166247306e8de735dc7cfdfc
matsushimayuki5225 <58546697+matsushimayuki5225@users.noreply.github.com>
2020-01-28 20:13:33 +0900

    変更１５

--- db/migrate/20200109030031_create_posts.rb
+++ db/migrate/20200109030031_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.text :title
-      t.text :content
+      t.text :message ,null: false
+      t.string :nickname ,null: false
       t.timestamps
     end
   end
